4c783253f4fe394c98cc7279f2fb3d1a02ace4f3
nishant aggarwal <nishantaggar@gmail.com>
2025-07-10 14:04:55 -0400

    exit log code

--- demo/src/main/java/com/tsc/model/ExitRecord.java
+++ demo/src/main/java/com/tsc/model/ExitRecord.java
@@ -6,40 +6,38 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
-
 @Entity
-public class Entry {
-
+public class ExitRecord {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
-    private String gate;
+    private String lot;
     private LocalDateTime timestamp;
     
-    
-	public Entry() {
+	public ExitRecord() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Entry(String gate, LocalDateTime timestamp) {
-		super();
+	
 
-		this.gate = gate;
-		this.timestamp = timestamp;
+	public ExitRecord(String lot) {
+		super();
+		this.lot = lot;
+		 this.timestamp = LocalDateTime.now();
 	}
+
+
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getGate() {
-		return gate;
+	public String getLot() {
+		return lot;
 	}
-	public void setGate(String gate) {
-		this.gate = gate;
+	public void setLot(String lot) {
+		this.lot = lot;
 	}
 	public LocalDateTime getTimestamp() {
 		return timestamp;
@@ -47,10 +45,7 @@ public class Entry {
 	public void setTimestamp(LocalDateTime timestamp) {
 		this.timestamp = timestamp;
 	}
-	@Override
-	public String toString() {
-		return "Entry [id=" + id + ", gate=" + gate + ", timestamp=" + timestamp + "]";
-	}
     
     
+
 }
